8db024cf33c5af07bfa663a3303dfc0a36c633f6
thienth32 <64114903+thienth32@users.noreply.github.com>
2021-11-11 12:41:00 +0700

    tìm kiếm

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,15 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index(){
-        $products = Product::where('price', '>', 5000)->get();
-        // select * from products where price > 5000
-        return view('products.index', compact('products'));
+    public function index(Request $request){
+        
+        $column_names = [
+            'name' => 'Tên sản phẩm',
+            'price' => 'Giá',
+            'star' => "Đánh giá",
+            'views' => 'Lượt xem'
+        ];
+
+        $order_by = [
+            'asc' => 'Tăng dần',
+            'desc' => 'Giảm dần'
+        ];
+
+
+        $keyword = $request->has('keyword') ? $request->keyword : "";
+        $cate_id = $request->has('cate_id') ? $request->cate_id : "";
+        $rq_order_by = $request->has('order_by') ? $request->order_by : 'asc';
+        $rq_column_names = $request->has('column_names') ? $request->column_names : "name";
+
+        // dd($keyword, $cate_id, $rq_column_names, $rq_order_by);
+        $query = Product::where('name', 'like', "%$keyword%");
+        if($rq_order_by == 'asc'){
+            $query->orderBy($rq_column_names);
+        }else{
+            $query->orderByDesc($rq_column_names);
+        }
+
+        if(!empty($cate_id)){
+            $query->where('cate_id', $cate_id);
+        }
+        $products = $query->get();
+
+        $categories = Category::all();
+        
+        
+        return view('products.index', compact('products', 'categories', 'column_names', 'order_by'));
     }
 
     public function remove($id){
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,3 +1,37 @@
+<form action="" method="get">
+    <div>
+        <label for="">Từ khóa</label>
+        <input type="text" name="keyword" placeholder="Tìm theo tên sản phẩm">
+    </div>
+    <div>
+        <label for="">Danh mục</label>
+        <select name="cate_id" >
+            <option value="">Tất cả</option>
+            @foreach ($categories as $item)
+                <option value="{{$item->id}}">{{$item->name}}</option>
+            @endforeach
+        </select>
+    </div>
+    <div>
+        <label for="">Tên cột</label>
+        <select name="column_names" >
+            @foreach ($column_names as $key => $item)
+                <option value="{{$key}}">{{$item}}</option>
+            @endforeach
+        </select>
+    </div>
+    <div>
+        <label for="">Sắp xếp theo</label>
+        <select name="order_by" >
+            @foreach ($order_by as $key => $item)
+                <option value="{{$key}}">{{$item}}</option>
+            @endforeach
+        </select>
+    </div>
+    <div>
+        <button type="submit">Tìm kiếm</button>
+    </div>
+</form>
 <table>
     <thead>
         <th>ID</th>
