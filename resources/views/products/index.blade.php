8d211fb2e914f980cec490b1df4b843c18097394
thienth32 <64114903+thienth32@users.noreply.github.com>
2021-11-20 13:40:49 +0700

    Update index.blade.php

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,76 +1,94 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-<form action="" method="get">
-    <div>
-        <label for="">Từ khóa</label>
-        <input type="text" name="keyword" value="{{$searchData['keyword']}}" placeholder="Tìm theo tên sản phẩm">
+@extends('admin.layouts.main')
+@section('content')
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <form action="" method="get">
+                    <div class="row">
+                        <div class="col-6">
+                            <div class="form-group">
+                                <label for="">Từ khóa</label>
+                                <input type="text" class="form-control" name="keyword" value="{{$searchData['keyword']}}" placeholder="Tìm theo tên sản phẩm">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Danh mục</label>
+                                <select name="cate_id" class="form-control">
+                                    <option value="">Tất cả</option>
+                                    @foreach ($categories as $item)
+                                        <option @if($item->id == $searchData['cate_id']) selected @endif value="{{$item->id}}">{{$item->name}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="form-group">
+                                <label for="">Tên cột</label>
+                                <select name="column_names" class="form-control">
+                                    @foreach ($column_names as $key => $item)
+                                        <option  @if($key == $searchData['column_names']) selected @endif value="{{$key}}">{{$item}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="">Sắp xếp theo</label>
+                                <select name="order_by" class="form-control">
+                                    @foreach ($order_by as $key => $item)
+                                        <option @if($key == $searchData['order_by']) selected @endif value="{{$key}}">{{$item}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 d-flex justify-content-end">
+                        <button class="btn btn-primary" type="submit">Tìm kiếm</button>
+                    </div>
+                </form>
+            </div>
+            <div class="card-body">
+                <table class="table table-stripped">
+                    <thead>
+                        <th>STT</th>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>Category</th>
+                        <th>Image</th>
+                        <th>Đã bán trong bao nhiêu hóa đơn</th>
+                        <th>
+                            <a href="">Add new</a>
+                        </th>
+                    </thead>
+                    <tbody>
+                        @foreach ($products as $item)
+                            <tr>
+                                <td>{{($products->currentPage() - 1)*$products->perPage() + $loop->iteration}}</td>
+                                <td>{{$item->name}}</td>
+                                <td>{{$item->price}}</td>
+                                <td>
+                                    <a href="{{route('cate.detail', ['id' => $item->category->id])}}">{{$item->category->name}}</a>
+                                </td>
+                                <td>
+                                    <img src="{{asset($item->image)}}" width="100">
+                                </td>
+                                <td>
+                                    {{count($item->orders)}}
+                                </td>
+                                <td>
+                                    <a href="{{route('product.edit', ['id' => $item->id])}}">Edit</a>
+                                    <a href="{{route('product.remove', ['id' => $item->id])}}">Remove</a>
+                                </td>
+                            </tr>
+                        @endforeach                        
+                    </tbody>
+                </table>
+                <div class="d-flex justify-content-center">
+                    {{$products->onEachSide(1)->links()}}
+                </div>
+            </div>
+        </div>
     </div>
-    <div>
-        <label for="">Danh mục</label>
-        <select name="cate_id" >
-            <option value="">Tất cả</option>
-            @foreach ($categories as $item)
-                <option @if($item->id == $searchData['cate_id']) selected @endif value="{{$item->id}}">{{$item->name}}</option>
-            @endforeach
-        </select>
-    </div>
-    <div>
-        <label for="">Tên cột</label>
-        <select name="column_names" >
-            @foreach ($column_names as $key => $item)
-                <option  @if($key == $searchData['column_names']) selected @endif value="{{$key}}">{{$item}}</option>
-            @endforeach
-        </select>
-    </div>
-    <div>
-        <label for="">Sắp xếp theo</label>
-        <select name="order_by" >
-            @foreach ($order_by as $key => $item)
-                <option @if($key == $searchData['order_by']) selected @endif value="{{$key}}">{{$item}}</option>
-            @endforeach
-        </select>
-    </div>
-    <div>
-        <button type="submit">Tìm kiếm</button>
-    </div>
-</form>
-<table>
-    <thead>
-        <th>STT</th>
-        <th>Name</th>
-        <th>Price</th>
-        <th>Category</th>
-        <th>Image</th>
-        <th>Đã bán trong bao nhiêu hóa đơn</th>
-        <th>
-            <a href="">Add new</a>
-        </th>
-    </thead>
-    <tbody>
-        @foreach ($products as $item)
-            <tr>
-                <td>{{($products->currentPage() - 1)*$products->perPage() + $loop->iteration}}</td>
-                <td>{{$item->name}}</td>
-                <td>{{$item->price}}</td>
-                <td>
-                    <a href="{{route('cate.detail', ['id' => $item->category->id])}}">{{$item->category->name}}</a>
-                </td>
-                <td>
-                    <img src="{{asset($item->image)}}" width="100">
-                </td>
-                <td>
-                    {{count($item->orders)}}
-                </td>
-                <td>
-                    <a href="{{route('product.edit', ['id' => $item->id])}}">Edit</a>
-                    <a href="{{route('product.remove', ['id' => $item->id])}}">Remove</a>
-                </td>
-            </tr>
-        @endforeach
-        <tr>
-            <td colspan="5">
-                {{$products->onEachSide(1)->links()}}
-            </td>
-        </tr>
-    </tbody>
-</table>
+</div>
+
+
+
+@endsection
